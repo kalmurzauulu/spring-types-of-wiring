0889691d625d88d2d5d0cc4c93e81f7471928f0a
ergeshmametuulukanat <ergeshmametuulukanat@gmail.com>
2021-03-31 00:20:19 +0600

    init12

---
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -0,0 +1,22 @@
+package peaksoft.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Island2;
+import peaksoft.model.Ocean1;
+import peaksoft.model.Rabbit4;
+import peaksoft.model.Wood3;
+
+@Configuration
+@ComponentScan(basePackages = "peaksoft")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland( Wood3 wood) {
+
+        return new Island2(wood);
+    }
+
+}
